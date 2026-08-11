0b8f62675c0f751b434e290e18883b2105a7f36c
Vitaliy Rudakovskiy <vitalirud03@gmail.com>
2026-08-11 13:28:51 +0300

    fix: update test environment to vitest

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { describe, beforeEach, expect, it } from 'vitest';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
